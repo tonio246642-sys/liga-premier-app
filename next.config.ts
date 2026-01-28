a12bde3f0f9b3c5c6407ba3dcf8e008dcb0d2629
tonio246642-sys <tonio246642@gmail.com>
2026-01-28 12:36:29 -0600

    Agregar credenciales y arreglar config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: any = {
+const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
-    // Advertencia: Esto permite que las compilaciones de producción se completen
-    // incluso si su proyecto tiene errores de ESLint.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // !! ADVERTENCIA !!
-    // Peligrosamente permite que las compilaciones de producción se completen
-    // incluso si su proyecto tiene errores de tipo.
     ignoreBuildErrors: true,
   },
 };
